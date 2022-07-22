13749cab673d682ec98b3c0f386c08bb29573d60
ahmed03214 <109301629+ahmed03214@users.noreply.github.com>
2022-07-22 18:22:20 +0200

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,8 +2,25 @@ import "../styles/global/globals.scss";
 import "../styles/global/helper.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Head from "next/head";
+import logo from "../public/assets/logo1.png";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="يمناك" />
+        <meta property="og:image" content={logo} />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="240" />
+        <meta property="og:image:height" content="240" />
+        <meta property="og:image:alt" content="Display Picture" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
