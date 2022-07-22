8d5f1bbf2c15e39274179346216c120e7937204e
ahmed03214 <109301629+ahmed03214@users.noreply.github.com>
2022-07-22 18:26:26 +0200

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,7 +3,6 @@ import "../styles/global/helper.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Head from "next/head";
-import logo from "../public/assets/logo1.png";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,7 +10,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta property="og:type" content="website" />
         <meta property="og:title" content="يمناك" />
-        <meta property="og:image" content={logo} />
+        <meta property="og:image" content="https://i.ibb.co/KGzsVXj/logo1.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="240" />
         <meta property="og:image:height" content="240" />
