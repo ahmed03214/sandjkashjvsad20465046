e83dea2529aaab7cccf0df0c42873a2d99ff6c7f
ahmed03214 <109301629+ahmed03214@users.noreply.github.com>
2022-07-25 17:09:34 +0200

    Add files via upload

--- layout/index.jsx
+++ layout/index.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 import Header from "./Header";
 import Footer from "./Footer";
+import ToTopBtn from "../components/ToTopBtn";
+import FloatingBtn from "../components/FloatingBtn";
 
 const UserLayout = ({ children, className = "", social, services }) => {
   return (
     <>
       <Header services={services} />
-      <main className={className}>{children}</main>
+      <main className={className}>
+        {children}
+
+        <ToTopBtn />
+        <FloatingBtn />
+      </main>
       <Footer social={social} />
     </>
   );
